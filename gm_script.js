7a0e2bc5b608c4f0a8c488a30699f1b2fc6801f3
poohcom1 <pooh677@gmail.com>
2024-06-04 00:37:28 +0700

    feat: added mock endpoint

--- gm_script.js
+++ gm_script.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name     Twitter Art Collection
 // @version  1
-// @grant    none
-// @version      0.1
-// @author       poohcom1
-// @match        https://x.com/*
-// @grant        none
+// @version  0.1
+// @author   poohcom1
+// @match    https://x.com/*
+// @grant    GM.xmlHttpRequest
 // ==/UserScript==
+
+// Environment
+const SERVER_URL = 'http://localhost:5757';
 
 // Assets
 const TAG_SVG = `<svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1xvli5t r-1hdv0qi"><g><path d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5zM6.5 4c-.276 0-.5.22-.5.5v14.56l6-4.29 6 4.29V4.5c0-.28-.224-.5-.5-.5h-11z"></path></g></svg>`;
-
 
 // General
 
@@ -29,67 +30,37 @@ async function waitForElement(selector, root = document) {
   });
 }
 
-async function waitFor(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-// Sensitive selectors
-const ROOT_PARENT_LABEL = '[aria-label="Timeline: Your Home Timeline"]';
-
-/**
- * @returns {Promise<HTMLElement>}
- */
-async function getTimelineRoot() {
-  const timelineRootParent = await waitForElement(ROOT_PARENT_LABEL);
-  return timelineRootParent.childNodes[0];
-}
-
-// Tweet helper
-
-/** @param {HTMLElement} tweet */
-function isTweet(tweet) {
-  return tweet.querySelector('article[data-testid="tweet"]') !== null;
-}
-
-/** @param {HTMLElement} tweet */
-function getTweetId(tweet) {
-  // TODO
-  tweet.querySelector('a')
-}
-
-/** @param {HTMLElement} tweet */
-function getTweetImages(tweet) {
-  return Array.from(tweet.querySelectorAll('img'))
-    .map((img) => img.src);
-}
-
-/** @param {HTMLElement} tweet */
-async function getTweetMenuContainer(tweet) {
-  const bookmarkButton = await waitForElement('[data-testid="bookmark"]', tweet);
-  return bookmarkButton.parentElement.parentElement;
-}
-
 // Menu helper
 
-// Main
-async function renderTweet(tweet) {
-  console.log(tweet)
-  const menuContainer = await getTweetMenuContainer(tweet);
+// API
+function addTag(id, tag, imageSrcs) {
+  const data = {
+    id: id,
+    tag: tag,
+    imageSrcs: imageSrcs
+  };
 
-  const menuButton = document.createElement('div');
-  menuButton.innerHTML = `
-<div>
-  <label for="tags">Tags</label>
-  <select name="tags" id="tags">
-    <option>Save</option>
-    <option>Report</option>
-  </select>
-</div>
-        `
-
-  menuContainer.appendChild(menuButton)
+  GM.xmlHttpRequest({
+    method: 'POST',
+    url: SERVER_URL + '/api/addTag',
+    data: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    onload: function (response) {
+      if (response.status === 200) {
+        console.log('Tag added successfully:', response.responseText);
+      } else {
+        console.error('Error adding tag:', response.status, response.responseText);
+      }
+    },
+    onerror: function (response) {
+      console.error('Request failed:', response);
+    }
+  });
 }
 
+// Main
 (async function () {
   'use strict';
   ``
@@ -131,7 +102,7 @@ async function renderTweet(tweet) {
       tagContainer.querySelector('svg').outerHTML = TAG_SVG;
       tagContainer.id = tagId;
       tagContainer.addEventListener('click', async () => {
-        alert("Tagging!");
+        addTag(id, 'test', ['image1', 'image2']);
       });
       dropdown.prepend(tagContainer);
     });
